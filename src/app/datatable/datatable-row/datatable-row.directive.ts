9f3ca6cb1c72df3c0d563573ac0da09a3b1901bd
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-09-05 08:39:45 +0200

    Basic setup

--- src/app/datatable/datatable-row/datatable-row.directive.ts
+++ src/app/datatable/datatable-row/datatable-row.directive.ts
@@ -1,10 +1,11 @@
 import { Directive } from '@angular/core';
+import { DatatableComponent } from '../datatable/datatable.component';
 
 @Directive({
   selector: '[appDatatableRow]'
 })
-export class DatatableRowDirective {
+export class DatatableRowDirective<TData> {
 
-  constructor() { }
+  constructor(public datatable: DatatableComponent<TData>) { }
 
 }
