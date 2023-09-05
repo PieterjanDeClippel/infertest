170072d870bada1934f08bf4b371ca446e480352
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-09-05 09:05:16 +0200

    Failed attempt

--- src/app/datatable/datatable-row/datatable-row.directive.ts
+++ src/app/datatable/datatable-row/datatable-row.directive.ts
@@ -1,11 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef } from '@angular/core';
 import { DatatableComponent } from '../datatable/datatable.component';
+
+class DatatableRowContext<T> {
+  constructor(public $implicit: T, public templateRef: TemplateRef<DatatableRowContext<T>>) {}
+  // $implicit!: T;
+
+  // constructor(value: T) {
+  //   this.$implicit = value;
+  // }
+}
 
 @Directive({
   selector: '[appDatatableRow]'
 })
 export class DatatableRowDirective<TData> {
 
-  constructor(public datatable: DatatableComponent<TData>) { }
+  constructor(public datatable: DatatableComponent<TData>, public template: TemplateRef<TData>) { }
+
+  static ngTemplateContextGuard<TData>(dir: DatatableRowDirective<TData>, ctx: unknown)
+    : ctx is DatatableRowContext<TData> {
+      return true;
+    }
 
 }
